f5308767ab4dd20bbf74ea2314fc28678a1ea6bb
gregflood918 <gregflood918@gmail.com>
2017-03-25 11:04:14 -0500

    Dies Irae and Gregory IX
    Added dies irae and gregory ix retrieval functions.  Also added db
    insertion functions.  ‘insertClaudius’ worked, however, I experience a
    concurrency error when I tried to insert a different function into the
    database

--- project1/project1.py
+++ project1/project1.py
@@ -27,16 +27,23 @@ import os
 import random
 
 #We will start with Claudius Caesar (smaller collection)
-
-links={"Aquinas":"http://thelatinlibrary.com/aquinas.html",
+#Gregory the 9th
+#Dies Ires
+links={"Gregory IX":"http://www.thelatinlibrary.com/gregory.html",
        "Claudius Caesar":"http://thelatinlibrary.com/claud.inscr.html",
        "Bernard of Clairvaux":"http://www.thelatinlibrary.com/bernardclairvaux.shtml",
        "Roman Epitaphs":"http://www.thelatinlibrary.com/epitaphs.html",
        "Anselm":"http://www.thelatinlibrary.com/anselm.html",
+       "Dies Irae":"http://www.thelatinlibrary.com/diesirae.html",
        "Christian Creeds":"http://www.thelatinlibrary.com/creeds.html",
        "Sulpicius Severus":"http://www.thelatinlibrary.com/sulpiciusseverus.html"}
  
        
+'''
+Database function.  Creates a database and a utility function
+for inserting contents into the database
+'''       
+             
 def createDB():
 #Function to create database that will be populated by elements of the
 #parsed latin text
@@ -47,14 +54,30 @@ def createDB():
         (title TEXT,book TEXT,language TEXT,author TEXT,
         dates TEXT,chapter TEXT,verse TEXT, passage TEXT,
         link TEXT)''')   
-        conn.commit()
-        conn.close()
+        c.commit()
+        c.close()
         return         
     except:
         print("Table Already Exists")
         return
         
-        
+def populateDB(records):    
+    conn = sqlite3.connect(r"latintext.db")
+    c = conn.cursor()
+    #Loop through array length
+    for i in range(len(records)):
+        c.execute("INSERT INTO latintext VALUES (?,?,?,?,?,?,?,?,?)",
+                  (records[i][0],records[i][1],records[i][2],records[i][3],\
+                   records[i][4],records[i][5],records[i][6],records[i][7],records[i][8]))
+    c.commit()
+    c.close()
+    return
+
+    
+'''
+Text manipulation utility function.  One function cleans the text and 
+returns formatted text.  Other parses text for hrefs.
+'''
 
 def cleanText(soup):
 #Utility function that returns a cleaned list of text from a BeautifulSoup
@@ -73,7 +96,6 @@ def cleanText(soup):
 
     return textList
     
-    
 def getLinks(soup):
 #Utility function that returns links on a page
     links=[]
@@ -84,7 +106,12 @@ def getLinks(soup):
         links.append(j['href'])
     return links
 
-    
+  
+'''
+Text Scrapers.  8 functions that access individual latin collections and
+returns their contents as a list of lists
+'''   
+       
 def fetchClaudius():
 #Function that retrives parses the Claudius Beautiful Soup object.  Returns
 # a list of list, where each inner list consists of the seven elements 
@@ -120,8 +147,8 @@ def fetchClaudius():
         returnList.append(innerList)
         paragraphNum +=1
     return returnList    
-        
-
+   
+     
 def fetchCreeds():
 #Function that retrives parses the Early Christian Creeds.  Returns
 # a list of list, where each inner list consists of the seven elements 
@@ -162,12 +189,6 @@ def fetchCreeds():
         returnList.append(innerList)
         verseNum +=1
     return returnList   
-    
-    
-'''CREATE TABLE latintext
-        (title TEXT,book TEXT,language TEXT,author TEXT,
-        dates TEXT,chapter TEXT,verse TEXT, passage TEXT,
-        link TEXT)'''     
     
          
 def fetchEpitaphs():
@@ -279,8 +300,7 @@ def fetchAnselm():
         
     return returnList
 
-    
-    
+        
 def fetchSeverus():
 #Functin that fetches text from the collection of works by Sulpicius Severus
 #on the latin library.  Returns a list of list in the format defined in the
@@ -331,12 +351,9 @@ def fetchSeverus():
                      innerS,("http://thelatinlibrary.com/" + testLinks[1])]
             verseNum += 1
             returnList.append(innerList)       
-    return returnList       
+    return returnList        #Possibly may have to come back here and cast the versenum to a string
         
-    #Possibly may have to come back here and cast the versenum to a string
-        
-    
-    
+   
 def fetchBernard():  
 #Function that retrieves the works of St. Bernard of Clairvaux from the
 #latin library and returns a list of list formatted per the database
@@ -351,7 +368,7 @@ def fetchBernard():
     soup = cleanText(soup)  #use clean text method
     
     author = "Bernard of Clairvaux"
-    date = re.search(r'\(.*\)',myList[1]).group(0)
+    date = re.search(r'\(.*\)',soup[1]).group(0)
     date = re.sub(r'\(|\)',"",date)
     verseNum = ""
     returnList = []
@@ -371,12 +388,185 @@ def fetchBernard():
         returnList.append(innerList)
     return returnList
 
-                   
-                   
-                   
-                   
-                   
-                   
-                   
-#soup.find('div',{"class":"ProfileHeaderCard"})
+                
+def fetchDiesIrae():  
+#Function that retrieves the poem (?) 'Dies Irae' from the
+#latin library and returns a list of list formatted per the database
+#schema of sqlite createDB function
+    global links
+    currLink = links["Dies Irae"] 
+    f = urllib.request.urlopen(currLink).read().decode('utf-8','ignore')
+    soup = BeautifulSoup(f,'html.parser')
+    avoid = r'The|Christian' #To avoid links
+    title = soup.title.text.strip()
+    book = "" #No books in this collection
+    author = "" #No author for this poem appears in html
+    date = "" #No obvious date in this collection
+    chapter = "" #No chapters in this collection.  This appears to be a poem
+    soup = cleanText(soup)  #use clean text method
+    soup = soup[1:] #remove redundant title
+    verseNum = 1 #Each line will be considered a verse
+    returnList = []
+    innerList = []
+     
+    #Loop through cleaned text                  
+    for i in soup:
+        if re.search(avoid,i):
+            continue
+        innerList = [title,book,"Latin",author,date,chapter,verseNum,
+                     i,currLink]
+        returnList.append(innerList)
+        verseNum +=1
+    return returnList    
+
+   
+def fetchGregory():
+#Function that retrieves the decrees of Pope Gregory IX.  There are 5 large books 
+#total.  All 4 books are returned within a single list of lists in the format of
+#the sqlite database.  The verse number gives teh chapter from the text, followed
+#by the paragraph number within that subsection.  This is more helpful if trying
+#to find something in the text
+    global links
+    currLink = links['Gregory IX']
+    f = urllib.request.urlopen(currLink).read().decode('utf-8','ignore')
+    soup = BeautifulSoup(f,'html.parser')
+    avoid = r'The|Christian|(FINIS LIBRI)' #To avoid links
+    title = soup.title.text.strip() #Pull out title
+    
+    dates="" #No apparent dates in the html
+    author= "Gregory IX" #Obvious  
+    #get links
+    testLinks = getLinks(soup)
+    soupsOn = []
+    for l in testLinks:
+        f = urllib.request.urlopen("http://thelatinlibrary.com/" + l).\
+        read().decode('utf-8','ignore')
+        soupsOn.append(BeautifulSoup(f,'html.parser'))
+
+    returnList = []
+    innerList = []
+    chapterMatch = r'TITULUS|GREGORIxUS'
+    verseMatch = r'CAP\.* *[A-Z]+\.*'
+    chapter = ""
+    verseNum = 1
+    verse = ""
+    versePrefix = "" #trying to create more helpful verse numbers
+
+    #nested for loop to cycle through each link
+    #all text from each link is added to the same list of lists
+    for s in soupsOn:  
+        print(type(s))
+        #Get book name
+        pageHeads = s.find("p",class_="pagehead")
+        print(pageHeads)
+        pageHeads = re.sub('<.*?>',"\n",pageHeads.text) #Remove tags
+        pageHeads = pageHeads.split('\n')
+        book = " ".join(pageHeads).strip()
+
+        #Manual parsing of text.  CleanText function not appropriate for
+        #this particular format
+        temp = s.text
+        temp = re.sub(r' +',' ',temp) #replace multiple spaces with single space
+        temp = re.sub(r'\xa0','\n',temp) #replace \xa0 w/ \n
+        temp = temp.split(('\n')) #split on newlines
+        textList = [x for x in temp if x] #remove empty elements
+        textList = [x for x in textList if x != " "] #remove space elements
+        textList = [x.strip() for x in textList if x!= " "] 
+        verseNum = 1
+        #Inner loop
+        for inner in textList:
+            if re.search(avoid,inner):  #check if it is an unnecesarry href
+                continue
+            if re.search(chapterMatch,inner):
+                chapter = inner
+                verseNum = 1
+                continue
+            if re.search(verseMatch,inner):
+                versePrefix = inner
+                verseNum = 1
+                continue
+            if not chapter: #skips the pageheader.  won't go until it has a chapter name
+                continue
+            verse = str(versePrefix) + " : " + str(verseNum) #More helpful numbers
+            innerList = [title,book,"Latin",author,dates,chapter,verse,
+                     inner,("http://thelatinlibrary.com/" + testLinks[1])]
+            verseNum += 1
+            returnList.append(innerList)       
+    return returnList       
+    
+
+'''
+Insertion functions.  Calls scrapers and inserts their contents into the
+database
+
+'''    
+    
+def insertClaudius():
+#Simple function to insert Claudius text into database
+    text = fetchClaudius()
+    populateDB(text)
+    return
+               
+def insertCreeds():
+#Simple function to insert Creeds text into database
+    text = fetchCreeds()
+    populateDB(text)
+    return
+ 
+def insertEpitaphs():
+#Simple function to insert Epitaphs into database
+    text = fetchEpitaphs()
+    populateDB(text)
+    return
+
+def insertDiesIrae():
+#Simple function to insert Dies Irae into database
+    text = fetchDiesIrae()
+    populateDB(text)
+    return
+
+def insertAnselm():
+#Simple function to insert Anselm into database
+    text = fetchAnselm()
+    populateDB(text)
+    return
+    
+def insertSeverus():
+#Simple function to insert Severus into database
+    text = fetchSeverus()
+    populateDB(text)
+    return
+    
+def insertBernard():
+#Simple function to insert St. Bernard into database
+    text = fetchBernard()
+    populateDB(text)
+    return
+    
+def insertGregory():
+#Simple function to insert St. Gregory IX into database
+    text = fetchGregory()
+    populateDB(text)
+    return
+
+    
+def status():
+#Function to randomly select contents from the DB.  This is for testing
+#purposes, and exactly mirrors code form normandb project   
+    conn = sqlite3.connect(r"latintext.db")
+    c = conn.cursor()
+    c.execute("SELECT COUNT(*) FROM latintext")
+    dbLength=c.fetchone()[0]
+    #Print db length
+    print("Total Number of Rows: ",dbLength)  
+    
+    #Grab 5 random rows
+    for i in range(5):
+        c.execute("SELECT book,verse FROM latintext ORDER BY RANDOM() LIMIT 1")
+        print(c.fetchall())
+        
+    conn.close()
+    
+    return
+
     
